ace207c2b3533e86a396dbe7461eb3c0d39010c7
Rajendra <itsrajendrapatel@gmail.com>
2023-03-24 10:37:00 +0530

    Avatar Page Done, Now working on Home Page

--- client/src/Pages/Avatar.jsx
+++ client/src/Pages/Avatar.jsx
@@ -1,9 +1,4 @@
 import React from "react";
-import two from "../Assets/two.png";
-import three from "../Assets/three.png";
-import four from "../Assets/four.png";
-import five from "../Assets/five.png";
-import six from "../Assets/six.png";
 import "../Styles/Avatar.css";
 
 const Avatar = () => {
@@ -12,11 +7,26 @@ const Avatar = () => {
       <div className="avatarDiv">
         <h1>Choose Avatar</h1>
         <div className="avatarImg">
-          <img src={two} alt="two" />
-          <img src={three} alt="one" />
-          <img src={four} alt="one" />
-          <img src={five} alt="one" />
-          <img src={six} alt="one" />
+          <img
+            src="https://res.cloudinary.com/diverse/image/upload/v1679633801/diverse/sbbvjvjlkrdfxas2qfjz.png"
+            alt="two"
+          />
+          <img
+            src="https://res.cloudinary.com/diverse/image/upload/v1679633793/diverse/r8ke9awmk1tn2ybny0na.png"
+            alt="one"
+          />
+          <img
+            src="https://res.cloudinary.com/diverse/image/upload/v1679633761/diverse/ttitom6r3zrvetbokven.png"
+            alt="one"
+          />
+          <img
+            src="https://res.cloudinary.com/diverse/image/upload/v1679633724/diverse/xctj387ja7iqdp8vvoub.png"
+            alt="one"
+          />
+          <img
+            src="https://res.cloudinary.com/diverse/image/upload/v1679633786/diverse/xvxsh11gdpxqmo6wmbxs.png"
+            alt="one"
+          />
         </div>
         <button>Selected</button>
       </div>
