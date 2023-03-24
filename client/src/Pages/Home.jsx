aba72e7ad2a69f146b45d06be6d3904b0bbd0e44
Rajendra <itsrajendrapatel@gmail.com>
2023-03-24 14:39:32 +0530

    Home Page Design Done

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,11 +1,250 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
+import "../Styles/Home.css";
+import { GrEdit } from "react-icons/gr";
+import { AiFillDelete } from "react-icons/ai";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <p>lorem20000</p>
+      <div className="mainTaskDiv">
+        <div className="singleTaskDiv">
+          <h1>Bug</h1>
+          <div className="singleDiv">
+            <div className="firstDiv">
+              <p>status</p>
+              <div>
+                <GrEdit />
+                <AiFillDelete />
+              </div>
+            </div>
+            <p>This is my work to be done by me only, noone will touch it.</p>
+            <p>Rajendra Patel</p>
+          </div>
+          <div className="singleDiv">
+            <div className="firstDiv">
+              <p>status</p>
+              <div>
+                <GrEdit />
+                <AiFillDelete />
+              </div>
+            </div>
+            <p>This is my work to be done by me only, noone will touch it.</p>
+            <p>Rajendra Patel</p>
+          </div>
+          <div className="singleDiv">
+            <div className="firstDiv">
+              <p>status</p>
+              <div>
+                <GrEdit />
+                <AiFillDelete />
+              </div>
+            </div>
+            <p>This is my work to be done by me only, noone will touch it.</p>
+            <p>Rajendra Patel</p>
+          </div>
+          <div className="singleDiv">
+            <div className="firstDiv">
+              <p>status</p>
+              <div>
+                <GrEdit />
+                <AiFillDelete />
+              </div>
+            </div>
+            <p>This is my work to be done by me only, noone will touch it.</p>
+            <p>Rajendra Patel</p>
+          </div>
+        </div>
+        <div className="singleTaskDiv">
+          <h1>Bug</h1>
+          <div className="singleDiv">
+            <div className="firstDiv">
+              <p>status</p>
+              <div>
+                <GrEdit />
+                <AiFillDelete />
+              </div>
+            </div>
+            <p>This is my work to be done by me only, noone will touch it.</p>
+            <p>Rajendra Patel</p>
+          </div>
+          <div className="singleDiv">
+            <div className="firstDiv">
+              <p>status</p>
+              <div>
+                <GrEdit />
+                <AiFillDelete />
+              </div>
+            </div>
+            <p>This is my work to be done by me only, noone will touch it.</p>
+            <p>Rajendra Patel</p>
+          </div>
+          <div className="singleDiv">
+            <div className="firstDiv">
+              <p>status</p>
+              <div>
+                <GrEdit />
+                <AiFillDelete />
+              </div>
+            </div>
+            <p>This is my work to be done by me only, noone will touch it.</p>
+            <p>Rajendra Patel</p>
+          </div>
+          <div className="singleDiv">
+            <div className="firstDiv">
+              <p>status</p>
+              <div>
+                <GrEdit />
+                <AiFillDelete />
+              </div>
+            </div>
+            <p>This is my work to be done by me only, noone will touch it.</p>
+            <p>Rajendra Patel</p>
+          </div>
+        </div>
+        <div className="singleTaskDiv">
+          <h1>Bug</h1>
+          <div className="singleDiv">
+            <div className="firstDiv">
+              <p>status</p>
+              <div>
+                <GrEdit />
+                <AiFillDelete />
+              </div>
+            </div>
+            <p>This is my work to be done by me only, noone will touch it.</p>
+            <p>Rajendra Patel</p>
+          </div>
+          <div className="singleDiv">
+            <div className="firstDiv">
+              <p>status</p>
+              <div>
+                <GrEdit />
+                <AiFillDelete />
+              </div>
+            </div>
+            <p>This is my work to be done by me only, noone will touch it.</p>
+            <p>Rajendra Patel</p>
+          </div>
+          <div className="singleDiv">
+            <div className="firstDiv">
+              <p>status</p>
+              <div>
+                <GrEdit />
+                <AiFillDelete />
+              </div>
+            </div>
+            <p>This is my work to be done by me only, noone will touch it.</p>
+            <p>Rajendra Patel</p>
+          </div>
+          <div className="singleDiv">
+            <div className="firstDiv">
+              <p>status</p>
+              <div>
+                <GrEdit />
+                <AiFillDelete />
+              </div>
+            </div>
+            <p>This is my work to be done by me only, noone will touch it.</p>
+            <p>Rajendra Patel</p>
+          </div>
+        </div>
+        <div className="singleTaskDiv">
+          <h1>Bug</h1>
+          <div className="singleDiv">
+            <div className="firstDiv">
+              <p>status</p>
+              <div>
+                <GrEdit />
+                <AiFillDelete />
+              </div>
+            </div>
+            <p>This is my work to be done by me only, noone will touch it.</p>
+            <p>Rajendra Patel</p>
+          </div>
+          <div className="singleDiv">
+            <div className="firstDiv">
+              <p>status</p>
+              <div>
+                <GrEdit />
+                <AiFillDelete />
+              </div>
+            </div>
+            <p>This is my work to be done by me only, noone will touch it.</p>
+            <p>Rajendra Patel</p>
+          </div>
+          <div className="singleDiv">
+            <div className="firstDiv">
+              <p>status</p>
+              <div>
+                <GrEdit />
+                <AiFillDelete />
+              </div>
+            </div>
+            <p>This is my work to be done by me only, noone will touch it.</p>
+            <p>Rajendra Patel</p>
+          </div>
+          <div className="singleDiv">
+            <div className="firstDiv">
+              <p>status</p>
+              <div>
+                <GrEdit />
+                <AiFillDelete />
+              </div>
+            </div>
+            <p>This is my work to be done by me only, noone will touch it.</p>
+            <p>Rajendra Patel</p>
+          </div>
+        </div>
+        <div className="singleTaskDiv">
+          <h1>Bug</h1>
+          <div className="singleDiv">
+            <div className="firstDiv">
+              <p>status</p>
+              <div>
+                <GrEdit />
+                <AiFillDelete />
+              </div>
+            </div>
+            <p>This is my work to be done by me only, noone will touch it.</p>
+            <p>Rajendra Patel</p>
+          </div>
+          <div className="singleDiv">
+            <div className="firstDiv">
+              <p>status</p>
+              <div>
+                <GrEdit />
+                <AiFillDelete />
+              </div>
+            </div>
+            <p>This is my work to be done by me only, noone will touch it.</p>
+            <p>Rajendra Patel</p>
+          </div>
+          <div className="singleDiv">
+            <div className="firstDiv">
+              <p>status</p>
+              <div>
+                <GrEdit />
+                <AiFillDelete />
+              </div>
+            </div>
+            <p>This is my work to be done by me only, noone will touch it.</p>
+            <p>Rajendra Patel</p>
+          </div>
+          <div className="singleDiv">
+            <div className="firstDiv">
+              <p>status</p>
+              <div>
+                <GrEdit />
+                <AiFillDelete />
+              </div>
+            </div>
+            <p>This is my work to be done by me only, noone will touch it.</p>
+            <p>Rajendra Patel</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
